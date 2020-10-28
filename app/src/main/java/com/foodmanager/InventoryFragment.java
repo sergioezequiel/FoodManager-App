2b51cc8d0592ae16d915b7fef405d564d5559bac
AlexFilipe98 <AlexFilipe98@users.noreply.github.com>
2020-10-28 18:07:00 +0000

    Adicionado o swipe gesture
    Nesta versao ja eh possivel mudar o menu so com o gesto de swipe para tornar a applicacao mais interativa

--- app/src/main/java/com/foodmanager/InventoryFragment.java
+++ app/src/main/java/com/foodmanager/InventoryFragment.java
@@ -1,5 +1,6 @@
 package com.foodmanager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -7,24 +8,94 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
 
 public class InventoryFragment extends Fragment {
 
+    private FloatingActionButton fab_main, fab1_mail, fab2_share;
+    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
+
+    Boolean isOpen = false;
+
+    @SuppressLint("ClickableViewAccessibility")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         View root = inflater.inflate(R.layout.fragment_inventory, container, false);
 
         return root;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fab_main = view.findViewById(R.id.fab);
+        fab1_mail = view.findViewById(R.id.fab1);
+        fab2_share = view.findViewById(R.id.fab2);
+        fab_close = AnimationUtils.loadAnimation(getContext(), R.anim.fab_close);
+        fab_open = AnimationUtils.loadAnimation(getContext(), R.anim.fab_open);
+        fab_clock = AnimationUtils.loadAnimation(getContext(), R.anim.fab_rotate_clock);
+        fab_anticlock = AnimationUtils.loadAnimation(getContext(), R.anim.fab_rotate_anticlock);
+
+        fab_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (isOpen) {
+                    fab2_share.startAnimation(fab_close);
+                    fab1_mail.startAnimation(fab_close);
+                    fab_main.startAnimation(fab_anticlock);
+                    fab2_share.setClickable(false);
+                    fab1_mail.setClickable(false);
+                    isOpen = false;
+                } else {
+                    fab2_share.startAnimation(fab_open);
+                    fab1_mail.startAnimation(fab_open);
+                    fab_main.startAnimation(fab_clock);
+                    fab2_share.setClickable(true);
+                    fab1_mail.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+        fab2_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Share", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        fab1_mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Email", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     @Override
@@ -43,4 +114,6 @@ public class InventoryFragment extends Fragment {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/foodmanager/MainActivity.java
+++ app/src/main/java/com/foodmanager/MainActivity.java
@@ -1,10 +1,17 @@
 package com.foodmanager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,12 +19,14 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        final BottomNavigationView navView = findViewById(R.id.bottom_navigation);
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -28,6 +37,75 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+        final ConstraintLayout constraintLayout = this.findViewById(R.id.container);
+        constraintLayout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                /*Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();*/
+            }
+
+            public void onSwipeRight() {
+
+                int selectedItemId = navView.getSelectedItemId();
+                MenuItem selectedItem = navView.getMenu().findItem(selectedItemId);
+
+                switch (selectedItem.toString()){
+                    case "Inventory":
+                        navView.setSelectedItemId(R.id.navigation_profile);
+                        break;
+                    case "Recipes":
+                        navView.setSelectedItemId(R.id.navigation_inventory);
+                        break;
+
+                    case "Shopping List":
+                        navView.setSelectedItemId(R.id.navigation_recipes);
+                        break;
+
+                    case "Profile":
+                        navView.setSelectedItemId(R.id.navigation_shopping_list);
+                        break;
+                }
+
+              /*  Toast.makeText(MainActivity.this,"Right" , Toast.LENGTH_SHORT).show();*/
+            }
+
+            public void onSwipeLeft() {
+                int selectedItemId = navView.getSelectedItemId();
+                MenuItem selectedItem = navView.getMenu().findItem(selectedItemId);
+
+                switch (selectedItem.toString()){
+                    case "Inventory":
+                        navView.setSelectedItemId(R.id.navigation_recipes);
+                        break;
+                    case "Recipes":
+                        navView.setSelectedItemId(R.id.navigation_shopping_list);
+                        break;
+
+                    case "Shopping List":
+                        navView.setSelectedItemId(R.id.navigation_profile);
+                        break;
+
+                    case "Profile":
+                        navView.setSelectedItemId(R.id.navigation_inventory);
+                        break;
+                }
+                /*Toast.makeText(MainActivity.this,selectedItem.toString() , Toast.LENGTH_SHORT).show();*/
+
+            }
+            public void onSwipeBottom() {
+                /*Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();*/
+            }
+
+        });
     }
 
+    private int getSelectedItem(BottomNavigationView bottomNavigationView) {
+        Menu menu = bottomNavigationView.getMenu();
+        for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            if (menuItem.isChecked()) {
+                return menuItem.getItemId();
+            }
+        }
+        return 0;
+    }
 }
